cff422ffccbf531d648ce63a52c3b6dc9dd4d6b6
Mayank Kumar <mayankk2308@gmail.com>
2017-07-08 16:38:17 +0530

    Project initialization - first commit

---
+++ MKDataDetector/MKDataDetector.h
@@ -0,0 +1,19 @@
+//
+//  MKDataDetector.h
+//  MKDataDetector
+//
+//  Created by Mayank Kumar on 7/8/17.
+//  Copyright © 2017 Mayank Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKDataDetector.
+FOUNDATION_EXPORT double MKDataDetectorVersionNumber;
+
+//! Project version string for MKDataDetector.
+FOUNDATION_EXPORT const unsigned char MKDataDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKDataDetector/PublicHeader.h>
+
+
