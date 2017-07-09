0fccc15f971e60ab4735f1254f1157ee79192fd4
Mayank Kumar <mayankk2308@gmail.com>
2017-07-09 15:20:48 +0530

    Prepare README and LICENSE

---
+++ MKDataDetector/MKDataDetector.swift
@@ -0,0 +1,13 @@
+//
+//  MKDataDetector.swift
+//  MKDataDetector
+//
+//  Created by Mayank Kumar on 7/8/17.
+//  Copyright © 2017 Mayank Kumar. All rights reserved.
+//
+
+import Foundation
+
+class MKDataDetector {
+    
+}
---
+++ MKDataDetector/ResultType.swift
@@ -0,0 +1,13 @@
+//
+//  DataType.swift
+//  MKDataDetector
+//
+//  Created by Mayank Kumar on 7/8/17.
+//  Copyright © 2017 Mayank Kumar. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType {
+    case spelling, grammar, date, address, link, replacement, correction, phoneNumber, transitInformation
+}
