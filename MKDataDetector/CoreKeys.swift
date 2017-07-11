3148306a7ec312a7f81d60f8114309199b9a33b9
Mayank Kumar <mayankk2308@gmail.com>
2017-07-11 15:28:30 +0530

    Add relevant keys for Address + Transit components

---
+++ MKDataDetector/CoreKeys.swift
@@ -0,0 +1,26 @@
+//
+//  CoreKeys.swift
+//  MKDataDetector
+//
+//  Created by Mayank Kumar on 7/11/17.
+//  Copyright © 2017 Mayank Kumar. All rights reserved.
+//
+
+import Foundation
+
+public struct Address {
+    public static let name = NSTextCheckingNameKey
+    public static let city = NSTextCheckingCityKey
+    public static let zip = NSTextCheckingZIPKey
+    public static let street = NSTextCheckingStreetKey
+    public static let country = NSTextCheckingCountryKey
+    public static let phone = NSTextCheckingPhoneKey
+    public static let jobTitle = NSTextCheckingJobTitleKey
+    public static let state = NSTextCheckingStateKey
+    public static let organization = NSTextCheckingOrganizationKey
+}
+
+public struct Transit {
+    public static let airline = NSTextCheckingAirlineKey
+    public static let flight = NSTextCheckingFlightKey
+}
