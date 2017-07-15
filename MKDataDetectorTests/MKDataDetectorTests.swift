46ba9a36edde2a69325c8b6daaf8c8518ef5eaa0
Mayank Kumar <mayankk2308@gmail.com>
2017-07-15 17:51:08 +0530

    Add capability for testing internal functions

--- MKDataDetectorTests/MKDataDetectorTests.swift
+++ MKDataDetectorTests/MKDataDetectorTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-import MKDataDetector
+@testable import MKDataDetector // Internal functions can be independently tested
 
 class MKDataDetectorTests: XCTestCase {
     
@@ -16,6 +16,13 @@ class MKDataDetectorTests: XCTestCase {
     func testDateExtraction() {
         let textBody = "I have a program at 7pm this Sunday, which ends on 11am Monday"
         let results = dataDetectorService.extractDates(fromTextBody: textBody)
+        XCTAssert(results != nil, "Result must not be nil.")
+        XCTAssert(results!.count == 2, "2 results must be returned.")
+    }
+    
+    func testMultipleDateExtractions() {
+        let textBodies = ["Program", "Event at 9pm tomorrow", "11am party"]
+        let results = dataDetectorService.extractDates(fromTextBodies: textBodies)
         XCTAssert(results != nil, "Result must not be nil.")
         XCTAssert(results!.count == 2, "2 results must be returned.")
     }
