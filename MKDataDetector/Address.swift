d9bd3c6b71398056c99bdf3fd6f4f2292019ba19
Pinak Jalan <pinak.jalan@me.com>
2017-07-16 14:00:35 +0530

    Added some address convenience features

--- MKDataDetector/Address.swift
+++ MKDataDetector/Address.swift
@@ -7,15 +7,50 @@
 //
 
 import Foundation
+import CoreLocation
 
 extension MKDataDetectorService {
     
-    public func extractAddresses(fromTextBody textBody: String) -> [AddressAnalysisResult]? {
+    public func extractAddresses(fromTextBody textBody: String) -> [AddressAnalysisResult?]? {
+        
+        let result = (extractData(fromTextBody: textBody, withResultType: .address)! as [AnalysisResult<String>?])
+        processLocation(result)
+        
         return extractData(fromTextBody: textBody, withResultType: .address)
     }
     
-    public func extractAddresses(fromTextBodies textBodies: [String]) -> [[AddressAnalysisResult]?]? {
+    public func extractAddresses(fromTextBodies textBodies: [String]) -> [[AddressAnalysisResult?]?]? {
         return extractData(fromTextBodies: textBodies, withResultType: .address)
     }
     
+    public func processLocation(_ addresses: [AnalysisResult<String>?])
+    {
+        let geocoder = CLGeocoder()
+        var locations: [CLLocation]?
+        for address in addresses
+        {
+            geocoder.geocodeAddressString((address?.source)!) { (placemarks, error) in
+                // Process Response
+                let location = self.processResponse(withPlacemarks: placemarks, error: error)
+                locations?.append(location)
+            }
+            
+        }
+    }
+    
+    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) -> CLLocation {
+        
+        var location: CLLocation?
+        if let error = error {
+            print("Unable to Forward Geocode Address (\(error))")
+            print( "Unable to Find Location for Address")
+        }
+        else
+        {
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+        }
+        return location!
+}
 }
